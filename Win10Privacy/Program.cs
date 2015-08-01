41e265c9669e8f9dc5dac8eff8d7d6b2fc079a88
SunnyBat <Sunnybat@yahoo.com>
2015-08-01 00:55:23 -0700

    Added warning if program not run as Administrator

--- Win10Privacy/Program.cs
+++ Win10Privacy/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SecurityIdentifier = System.Security.Principal.SecurityIdentifier;
+using WellKnownSidType = System.Security.Principal.WellKnownSidType;
 
 namespace Win10Privacy
 {
@@ -14,9 +16,34 @@ namespace Win10Privacy
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // Is admin code basically copy+pasted from http://stackoverflow.com/questions/509292/how-can-i-tell-if-my-process-is-running-as-administrator
+            // Thanks casperOne!
+            var localAdminGroupSid = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null);
+            bool shouldRun = System.Security.Principal.WindowsIdentity.GetCurrent().Groups.
+                    Select(g => (SecurityIdentifier)g.Translate(typeof(SecurityIdentifier))).
+                    Any(s => s == localAdminGroupSid);
+            if (!shouldRun)
+            {
+                DialogResult result = MessageBox.Show("You must run this program as an Administrator! This program makes changes to the Windows Registry, "
+                    + "which requires Administrator access. You may continue, but it probably won't work!", "Windows 10 Privacy Tool",
+                    MessageBoxButtons.AbortRetryIgnore);
+                if (result == DialogResult.Abort)
+                {
+                    // Do nothing, it will exit normally
+                } else if (result == DialogResult.Retry)
+                {
+                    Main();
+                } else if (result == DialogResult.Ignore)
+                {
+                    shouldRun = true;
+                }
+            }
+            if (shouldRun)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
